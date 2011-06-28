8036134c55918de260857b2866609302b6f17215
twinther <tommy@winther.nu>
2011-06-28 21:18:15 +0200

    refactored and simplified

--- addon.py
+++ addon.py
@@ -1,123 +1,43 @@
-import re
-import pickle
 import os
+import re
 import sys
+import urllib2
 
-import xbmc
-import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 
-import feedparser
-import danishaddons
-import danishaddons.web
+VIDEO_PODCAST_URL = 'http://www.dr.dk/podcast/Video'
 
-BASE_URL = 'http://www.dr.dk/podcast/Video'
-FEED_URL_TEMPLATE = 'http://vpodcast.dr.dk/feeds/%s.xml'
+class DrDkPodcast(object):
+    def showOverview(self):
+        u = urllib2.urlopen(VIDEO_PODCAST_URL)
+        html = u.read()
+        u.close()
 
-def showOverview():
-    xbmcplugin.setContent(danishaddons.ADDON_HANDLE, 'tvshows')
+        icon = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
+        fanart = os.path.join(ADDON.getAddonInfo('path'), 'fanart.jpg')
 
-    html_path = os.path.join(danishaddons.ADDON_DATA_PATH, 'video.html')
-    html = danishaddons.web.downloadAndCacheUrl(BASE_URL, html_path, 24 * 60)
+        for m in re.finditer('<h2>.*?<a href="[^"]+">([^<]+)</a>\s+</h2>\s+<p>([^<]*)</p>.*?(http://vpodcast.dr.dk/feeds/.*?\\.xml)', html, re.DOTALL):
+            title = m.group(1)
+            description = m.group(2)
+            url = m.group(3)
 
-    dialog = xbmcgui.DialogProgress()
-    dialog.create(danishaddons.msg(30001))
+            item = xbmcgui.ListItem(title, iconImage = icon)
+            item.setProperty('Fanart_Image', fanart)
+            item.setInfo(type = 'video', infoLabels = {
+                'title' : title,
+                'plot' : description
+            })
+            url = url.replace('http://', 'rss://')
+            xbmcplugin.addDirectoryItem(HANDLE, url, item, True)
 
-    feed_count = html.count(FEED_URL_TEMPLATE[:15])
-    feed_idx = 0
-    for m in re.finditer(FEED_URL_TEMPLATE % '(.*)\\', html):
-        feed_idx+=1
-        key = m.group(1)
-        details = getFeedDetails(key)
-        if details is None:
-            continue
-
-        iconImage = None
-        if details.has_key('image_path'):
-            iconImage = details['image_path']
-
-        item = xbmcgui.ListItem(details['title'], iconImage = iconImage)
-        item.setInfo(type = 'video', infoLabels = {
-            'title' : details['title'],
-            'plot' : details['description']
-        })
-        url = danishaddons.ADDON_PATH + '?key=' + key
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, url, item, True)
-
-        dialog.update((feed_idx * 100 / feed_count), details['title'])
-        if dialog.iscanceled():
-            break
-
-    dialog.close()
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
-
-def showFeed(key):
-    d = feedparser.parse(FEED_URL_TEMPLATE % key)
-    details = getFeedDetails(key)
-
-    for e in d.entries:
-        item = xbmcgui.ListItem(e.title, iconImage = details['image_path'])
-        try:
-            description = e.description
-        except AttributeError:
-            description = 'No description'
-
-        infoLabels = {
-            'tvshowtitle' : d.channel.title,
-            'title' : e.title,
-            'plot' : description,
-            'duration' : e.itunes_duration,
-            'director' : e.author,
-            'writer' : e.author
-        }
-        if e.updated_parsed is not None:
-            infoLabels['aired'] = parsePubDate(e.updated_parsed)
-            infoLabels['year'] = e.updated_parsed[0]
-
-        item.setLabel2(description)
-        item.setInfo('video', infoLabels)
-        xbmcplugin.addDirectoryItem(danishaddons.ADDON_HANDLE, e.enclosures[0].href, item, False)
-
-    xbmcplugin.endOfDirectory(danishaddons.ADDON_HANDLE)
-
-    xbmcplugin.setContent(danishaddons.ADDON_HANDLE, 'episodes')
-    xbmcplugin.setPluginCategory(danishaddons.ADDON_HANDLE, d.channel.title)
-
-def getFeedDetails(key):
-    details_path = os.path.join(danishaddons.ADDON_DATA_PATH, key + '.pickle')
-    details = {}
-
-    if not os.path.exists(details_path):
-        d = feedparser.parse(FEED_URL_TEMPLATE % key)
-        if d.bozo == 1:
-            return None
-        
-
-        details['title'] = d.feed.title
-        details['description'] = d.feed.subtitle
-        if d.channel.image is not None:
-            details['image_path'] = d.channel.image.href
-
-        f = open(details_path, 'wb')
-        pickle.dump(details, f)
-        f.close()
-    else:
-        f = open(details_path, 'rb')
-        details = pickle.load(f)
-        f.close()
-
-    return details
-
-def parsePubDate(pubDate):
-    return '%d-%d-%d' % (pubDate[0], pubDate[1], pubDate[2])
+        xbmcplugin.endOfDirectory(HANDLE)
 
 
 if __name__ == '__main__':
-    danishaddons.init(sys.argv)
+    ADDON = xbmcaddon.Addon(id = 'plugin.video.dr.dk.podcast')
+    HANDLE = int(sys.argv[1])
 
-    if danishaddons.ADDON_PARAMS.has_key('key'):
-        showFeed(danishaddons.ADDON_PARAMS['key'])
-    else:
-        showOverview()
-
+    addon = DrDkPodcast()
+    addon.showOverview()
